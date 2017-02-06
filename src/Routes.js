9cf754b2111a66684606f18689ecc3ed3b084ac3
Benjamin Saphier <b.saphier@gmail.com>
2017-02-05 22:26:52 -0500

    added google fonts

--- src/Routes.js
+++ src/Routes.js
@@ -6,25 +6,14 @@ import {
 } from 'react-router';
 import React from 'react';
 
-import Main from './components/Main';
 import SectionWrapper from './components/SectionWrapper';
-
 import ResumeContainer from './containers/ResumeContainer';
-// import HomePageContainer from './containers/HomePageContainer';
-// import MonotronContainer from './containers/MonotronContainer';
-
 
 const Routes = () => (
   <Router history={browserHistory}>
-    {/* <Route path="/" component={Main}> */}
-      {/* <IndexRoute component={HomePageContainer} /> */}
-      {/* <IndexRoute component={SectionWrapper} /> */}
-      <Route path="/my-site" component={SectionWrapper}>
-        <IndexRoute component={ResumeContainer} />
-      </Route>
-      {/* <Route path="/monotron" component={MonotronContainer} />
-      <Route path="*" component={HomePageContainer} /> */}
-    {/* </Route> */}
+    <Route path="/my-site" component={SectionWrapper}>
+      <IndexRoute component={ResumeContainer} />
+    </Route>
   </Router>
 );
 
