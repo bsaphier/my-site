5c3d6f067a4bbbf4ee0657aa94a0a9d685244513
Benjamin Saphier <b.saphier@gmail.com>
2017-03-12 15:31:48 -0400

    better font sizing and safari-compat

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,8 +7,6 @@ const styles = {
     background: 'none',
     textAlign: 'center',
     color: 'rgba(255, 68, 62, 1)',
-    WebkitBackgroundClip: 'text',
-    // padding: '40pt',
     width: '100%',
     height: '100%',
     paddingTop: 0,
@@ -17,21 +15,24 @@ const styles = {
     position: 'absolute',
     top: '38%',
     left: '50%',
+    WebkitTransform: 'translate(-50%, -50%)',
     transform: 'translate(-50%, -50%)',
-    verticalAlign: 'middle',
+    willChange: 'transform'
   },
   title: {
     display: 'flex',
     textAlign: 'justify',
-    textTransform: 'uppercase',
+    textTransform: 'uppercase'
   },
   text: {
     margin: '0 auto',
     display: 'inline-block',
     whiteSpace: 'pre',
-    WebkitBackgroundClip: 'text',
     WebkitTextFillColor: 'transparent',
-    backgroundImage: 'linear-gradient(to top right, rgb(255, 115, 50) 62%, rgb(252, 255, 88) 162%)'
+    background: 'linear-gradient(to top right, rgb(255, 115, 50) 62%, rgb(252, 255, 88) 162%)',
+    MozBackgroundClip: 'text',
+    WebkitBackgroundClip: 'text',
+    backgroundClip: 'text'
   }
 };
 
@@ -60,13 +61,13 @@ const motion = {
   },
   enter: {
     top: spring(38, presets.stiff),
-    letterSpacing0: spring(-0.03, presets.wobbly),
-    letterSpacing1: spring(0.25, presets.gentle),
+    letterSpacing0: spring(-0.055, presets.wobbly),
+    letterSpacing1: spring(0.175, presets.gentle),
     letterSpacing2: spring(-0.06, presets.wobbly),
     letterSpacing3: spring(-0.08, presets.gentle),
-    letterSpacing4: spring(-0.05, presets.gentle),
+    letterSpacing4: spring(-0.048, presets.gentle),
     letterSpacing5: spring(-0.1, presets.gentle),
-    letterSpacing6: spring(-0.1, presets.gentle),
+    letterSpacing6: spring(-0.091, presets.gentle),
     letterSpacing7: spring(-0.1, presets.gentle)
   }
 };
@@ -108,25 +109,23 @@ class Banner extends Component {
   enterAnimate() {
     let nextState = { motion: {
           top: spring(38, presets.stiff),
-          letterSpacing0: spring(-0.03, presets.gentle),
-          letterSpacing1: spring(0.25, presets.gentle),
+          letterSpacing0: spring(-0.055, presets.gentle),
+          letterSpacing1: spring(0.175, presets.gentle),
           letterSpacing2: spring(-0.06, presets.wobbly),
           letterSpacing3: spring(-0.08, presets.gentle),
-          letterSpacing4: spring(-0.05, presets.gentle),
+          letterSpacing4: spring(-0.048, presets.gentle),
           letterSpacing5: spring(-0.1, presets.gentle),
-          letterSpacing6: spring(-0.1, presets.gentle),
+          letterSpacing6: spring(-0.091, presets.gentle),
           letterSpacing7: spring(-0.1, presets.gentle)
         }};
     this.setState({enter: true, ...nextState});
   }
 
   hover(id) {
-    console.log(id);
     this.setState({ motion: { ...motion.enter, [id]: motion.exit[id] }});
   }
 
   leave(id) {
-    console.log(id);
     this.setState({ motion: {...motion.enter, [id]: motion.enter[id] }});
   }
 
@@ -136,79 +135,118 @@ class Banner extends Component {
         <Motion style={this.state.motion}>
           {(interpStyle) => (
             <div id="cell" style={{...styles.cell, top: `${interpStyle.top}%`}}>
-              <Title hover={this.hover} leave={this.leave} id="letterSpacing0" style={{
-                fontWeight: 500,
-                fontSize: '14vh',
-                lineHeight: '11vh',
-                paddingRight: '0.07em',
-                letterSpacing: `${interpStyle.letterSpacing0}em`
-              }}>
+              <Title
+                hover={this.hover}
+                leave={this.leave}
+                id="letterSpacing0"
+                style={{
+                  fontWeight: 500,
+                  fontSize: 'calc(3.8 * 4.275vmin)',
+                  lineHeight: '0.7em',
+                  paddingTop: '0.06em',
+                  paddingRight: '0.1em',
+                  paddingLeft: '0.05em',
+                  letterSpacing: `${interpStyle.letterSpacing0}em`
+                }}>
                 Hello
               </Title>
-              <Title hover={this.hover} leave={this.leave} id="letterSpacing1" style={{
-                fontWeight: 100,
-                fontSize: '5vh',
-                lineHeight: '4vh',
-                letterSpacing: `${interpStyle.letterSpacing1}em`
-              }}>
+              <Title
+                hover={this.hover}
+                leave={this.leave}
+                id="letterSpacing1"
+                style={{
+                  fontWeight: 100,
+                  fontSize: 'calc(3.8 * 1.6vmin)',
+                  lineHeight: '0.8em',
+                  paddingTop: '0.06em',
+                  paddingLeft: '0.15em',
+                  letterSpacing: `${interpStyle.letterSpacing1}em`
+                }}>
                 My name is
               </Title>
-              <Title hover={this.hover} leave={this.leave} id="letterSpacing2" style={{
-                fontWeight: 900,
-                fontSize: '23vh',
-                lineHeight: '16.2vh',
-                paddingTop: '0.03em',
-                paddingRight: '0.1em',
-                letterSpacing: `${interpStyle.letterSpacing2}em`
-              }}>
-                <b style={{letterSpacing: `${interpStyle.letterSpacing2 - 0.02}em`}}>B</b>en
+              <Title
+                hover={this.hover}
+                leave={this.leave}
+                id="letterSpacing2"
+                style={{
+                  fontWeight: 900,
+                  fontSize: 'calc(3.8 * 6.89vmin)',
+                  lineHeight: '0.7em',
+                  paddingTop: '0.03em',
+                  paddingRight: '0.06em',
+                  paddingBottom: '0.01em',
+                  letterSpacing: `${interpStyle.letterSpacing2}em`
+                }}>
+                <b style={{letterSpacing: `${interpStyle.letterSpacing2 - 0.035}em`}}>B</b>en
               </Title>
-              <Title hover={this.hover} leave={this.leave} id="letterSpacing3" style={{
-                fontWeight: 100,
-                fontSize: '12vh',
-                fontStyle: 'italic',
-                lineHeight: '9.6vh',
-                paddingRight: '0.15em',
-                letterSpacing: `${interpStyle.letterSpacing3}em`
-              }}>
+              <Title
+                hover={this.hover}
+                leave={this.leave}
+                id="letterSpacing3"
+                style={{
+                  fontWeight: 100,
+                  fontSize: 'calc(3.8 * 3.5vmin)',
+                  fontStyle: 'italic',
+                  lineHeight: '0.8em',
+                  paddingRight: '0.1em',
+                  letterSpacing: `${interpStyle.letterSpacing3}em`
+                }}>
                 Saphier
               </Title>
-              <Title hover={this.hover} leave={this.leave} id="letterSpacing4" style={{
-                fontWeight: 900,
-                fontSize: '3.4vh',
-                lineHeight: '2.3vh',
-                wordSpacing: '-0.05em',
-                paddingTop: '0.07em',
-                paddingRight: '0.15em',
-                letterSpacing: `${interpStyle.letterSpacing4}em`
-              }}>
+              <Title
+                hover={this.hover}
+                leave={this.leave}
+                id="letterSpacing4"
+                style={{
+                  fontWeight: 900,
+                  fontSize: 'calc(3.8 * 1vmin)',
+                  lineHeight: '0.9em',
+                  wordSpacing: '-0.05em',
+                  paddingTop: '0.07em',
+                  paddingRight: '0.103em',
+                  letterSpacing: `${interpStyle.letterSpacing4}em`
+                }}>
                 I'm a software engineer
               </Title>
-              <Title hover={this.hover} leave={this.leave} id="letterSpacing5" style={{
-                fontWeight: 100,
-                fontSize: '6.5vh',
-                lineHeight: '5.7vh',
-                paddingRight: '0.175em',
-                letterSpacing: `${interpStyle.letterSpacing5}em`
-              }}>
+              <Title
+                hover={this.hover}
+                leave={this.leave}
+                id="letterSpacing5"
+                style={{
+                  fontWeight: 100,
+                  fontSize: 'calc(3.8 * 1.91vmin)',
+                  lineHeight: '0.9em',
+                  paddingRight: '0.175em',
+                  letterSpacing: `${interpStyle.letterSpacing5}em`
+                }}>
                 Exploring the
               </Title>
-              <Title hover={this.hover} leave={this.leave} id="letterSpacing6" style={{
-                fontWeight: 400,
-                fontSize: '4.5vh',
-                lineHeight: '3.6vh',
-                paddingRight: '0.18em',
-                letterSpacing: `${interpStyle.letterSpacing6}em`
-              }}>
+              <Title
+                hover={this.hover}
+                leave={this.leave}
+                id="letterSpacing6"
+                style={{
+                  fontWeight: 400,
+                  fontSize: 'calc(3.8 * 1.3vmin)',
+                  lineHeight: '0.7em',
+                  paddingTop: '0.08em',
+                  paddingRight: '0.13em',
+                  paddingBottom: '0.03em',
+                  letterSpacing: `${interpStyle.letterSpacing6}em`
+                }}>
                 crossover between
               </Title>
-              <Title hover={this.hover} leave={this.leave} id="letterSpacing7" style={{
-                fontWeight: 100,
-                fontSize: '6.66vh',
-                lineHeight: '7vh',
-                paddingRight: '0.2em',
-                letterSpacing: `${interpStyle.letterSpacing7}em`
-              }}>
+              <Title
+                hover={this.hover}
+                leave={this.leave}
+                id="letterSpacing7"
+                style={{
+                  fontWeight: 100,
+                  fontSize: 'calc(3.8 * 2vmin)',
+                  lineHeight: '1em',
+                  paddingRight: '0.2em',
+                  letterSpacing: `${interpStyle.letterSpacing7}em`
+                }}>
                 Sound & Code
               </Title>
             </div>
--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+// import getMuiTheme from 'material-ui/styles/getMuiTheme';
+// import injectTapEventPlugin from 'react-tap-event-plugin';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 
 import store from './store';
 import Routes from './Routes';
 
-injectTapEventPlugin();
+// injectTapEventPlugin();
 
 render(
-  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+  // <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
     <Provider store={store}>
       <Routes />
-    </Provider>
-  </MuiThemeProvider>,
+    </Provider>,
+  // </MuiThemeProvider>,
   document.getElementById('app')
 );
